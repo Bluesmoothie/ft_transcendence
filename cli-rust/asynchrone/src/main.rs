1382a13f0fdf90b3438288267fbfa195152faac1
Sths147 <simon.thomas.tux@gmail.com>
2025-11-20 18:58:17 +0100

    ongoing rust

---
+++ cli-rust/asynchrone/src/main.rs
@@ -0,0 +1,12 @@
+use trpl::Html;
+
+async fn page_title(url: &str) -> Option<String> {
+    let response_text = trpl::get(url).await.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title| title.inner_html())
+}
+
+fn main() {
+    println!("Hello, world!");
+}
